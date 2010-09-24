1bfe661747df49b4db32e28b7aa5a347ca6b1a29
Jason Grout <jason.grout@drake.edu>
2010-09-23 23:05:00 -0500

    Try to be even smarter about sending things to a target.

--- MyBot.py
+++ MyBot.py
@@ -49,33 +49,56 @@ def do_turn(pw):
   if len(pw.my_planets)==0:
     return
 
-  # identify my planets will have a surplus after 5 turns
-  surplus=set([pw.planets[p] for p in pw.my_planet_ids & pw_future[5].my_planet_ids])
+  target_planets=set(pw.planets)-pw.my_planets
+  #target_planets-=set(pw.planets[f.destination] for f in pw.my_fleets)
 
-  if len(surplus)>0:
-    surplus=sorted(surplus, key=lambda p: float(p.num_ships)/(p.future[5]-p.num_ships+1))
-    # (3) Find the weakest enemy or neutral planet, as long as I'm not
-    # already sending a fleet there.
-    target_planets=set(pw.planets)-pw.my_planets
-    target_planets-=set(pw.planets[f.destination] for f in pw.my_fleets)
-    
-    # get the top 5 targets
-    dest_score=lambda p: float(p.growth_rate)/(1+p.num_ships)/(1+min([pw.distance(i,p) for i in surplus]))
-    dest=sorted(target_planets, key=dest_score)[-2:]
-    if len(dest)>0:
-      #log('sending')
-      # (4) Send half the ships from my strongest planet to the weakest
-      # planet that I do not own.
-      for s in surplus[-2:]:
-#        ships_to_spare=ceil(min(0.9*s.num_ships,
-#                           (pw_future[5].planets[s.id].num_ships-s.num_ships)*3))
-        ships_to_spare=0.5*s.num_ships
-        if ships_to_spare>5:
-          d=min(dest, key=lambda p: pw.distance(p,s))
-          pw.order(s.id, d.id, ships_to_spare)
-          log('new fleet: %d %d: %d'%(s.id, d.id, ships_to_spare))
-          pw.new_fleets.add(Fleet(1, ships_to_spare, s.id, d.id, 
-                                  pw.distance(d,s), pw.distance(d,s)))
+  # get the top targets
+  dest_score=lambda p: float(p.growth_rate)/(1+p.num_ships)/(1+min([pw.distance(i,p) for i in pw.my_planets]))
+  target=sorted(target_planets, key=dest_score)[-2:]
+
+  # Now how can I conquer these planets?
+  for p in reversed(target):
+    # select 5 closest planets - what can they give up?
+    donors=[pw.planets[i] for i in p.my_close[:2]]
+    attack_fleets=[]
+    for q in donors:
+      if q.future[5]>q.num_ships:
+        # if we're growing on this planet, donate something
+        ships=int(q.future[0]*0.5)
+        if ships==0:
+          continue
+        attack_fleets.append((q,ships))
+    if sum(i[1] for i in attack_fleets)>1.2*p.future[5]:
+      for q, ships in attack_fleets:
+        pw.order(q,p, ships)
+        pw.new_fleets.add(Fleet(1, ships, q.id, p.id, 
+                                pw.distance(p,q), pw.distance(p,q)))
+        for i in range(len(q.future)):
+          q.future[i]-=ships
+        #q.num_ships-=ships
+
+#   # identify my planets will have a surplus after 5 turns
+#   surplus=set([pw.planets[p] for p in pw.my_planet_ids & pw_future[5].my_planet_ids])
+
+#   if len(surplus)>0:
+#     surpluskey=lambda p: float(p.num_ships)/( (p.future[5]-p.num_ships) if p.future[5]!=p.num_ships else 0.1)
+#     surplus=sorted(surplus, key=surpluskey)
+#     # (3) Find the weakest enemy or neutral planet, as long as I'm not
+#     # already sending a fleet there.
+#     if len(dest)>0:
+#       #log('sending')
+#       # (4) Send half the ships from my strongest planet to the weakest
+#       # planet that I do not own.
+#       for s in surplus[-2:]:
+# #        ships_to_spare=ceil(min(0.9*s.num_ships,
+# #                           (pw_future[5].planets[s.id].num_ships-s.num_ships)*3))
+#         ships_to_spare=0.5*s.num_ships
+#         if ships_to_spare>5:
+#           d=min(dest, key=lambda p: pw.distance(p,s))
+#           pw.order(s.id, d.id, ships_to_spare)
+#           log('new fleet: %d %d: %d'%(s.id, d.id, ships_to_spare))
+#           pw.new_fleets.add(Fleet(1, ships_to_spare, s.id, d.id, 
+#                                   pw.distance(d,s), pw.distance(d,s)))
 
   my_planets=copy(pw.my_planets)
   evacuate=set()
